0d31a244d5c1fdae2a31e3d056191b3fe6910af5
mrevanzak <mrevanzak@gmail.com>
2025-05-07 11:16:01 +0700

    chore: swift-format setup

--- willasnap/willasnapApp.swift
+++ willasnap/willasnapApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct willasnapApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
