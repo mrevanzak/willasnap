48a7af351589594af5cdd8e2851f92684d9a6380
mrevanzak <mrevanzak@gmail.com>
2025-05-07 10:08:25 +0700

    Initial Commit

---
+++ willasnap/willasnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  willasnapApp.swift
+//  willasnap
+//
+//  Created by Revanza Kurniawan on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct willasnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ willasnapTests/willasnapTests.swift
@@ -0,0 +1,16 @@
+//
+//  willasnapTests.swift
+//  willasnapTests
+//
+//  Created by Revanza Kurniawan on 07/05/25.
+//
+
+import Testing
+
+struct willasnapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
